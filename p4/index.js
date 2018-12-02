e73423546883b31739f4411ebd71da3409607a2e
Wilton Ngo <wiltonngo@gmail.com>
2018-12-02 15:48:29 -0500

    Refactored variables using destructuring

--- p4/index.js
+++ p4/index.js
@@ -4,19 +4,17 @@ let input = "input.txt";
 fs.readFile(input, 'utf8', function(err, data) {
     if (err) throw err;
     result = data.replace(/\r\n/g, '\n').split('\n');
-    
+
     for (let i = 0; i < result.length; i++) {
         for (let j = i + 1; j < result.length; j++) {
-            let sourceString = result[i];
-            let targetString = result[j];
+            let [sourceString, targetString] = [result[i], result[j]];
             for (let [k, isFound] = [0, false]; k < sourceString.length && !isFound; k++) {
                 if (sourceString[k] !== targetString[k]) {
                     isFound = true;
                     if (sourceString.substring(k+1, sourceString.length) === targetString.substring(k+1, targetString.length)) {
-                        let strBefore = sourceString.substring(0, k);
-                        let strAfter = sourceString.substring(k+1, sourceString.length);
-                        console.log(strBefore + strAfter);
-                        return strBefore + strAfter;
+                        let [strBeforek, strAfterk] = [sourceString.substring(0, k), sourceString.substring(k+1, sourceString.length)];
+                        console.log(strBeforek + strAfterk);
+                        return strBeforek + strAfterk;
                     }
                 } 
             }
