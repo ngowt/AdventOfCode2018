95e1d4431eca7c8e4b7feac4e8aa5a0e22aa0433
Wilton Ngo <wiltonngo@gmail.com>
2018-12-04 15:43:48 -0500

    Added solution to P7

---
+++ P7/index.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+let input = "input.txt";
+
+fs.readFile(input, 'utf8', function(err, data) {
+    if (err) throw err;
+    let dateRegex = /(?<=\[)(.*?)(?=\])/;
+    let actionRegex = /(?<=\]\s)(.*)/;
+    let idRegex = /(?<=\#)([0-9]*)/;
+    let result = data.replace(/\r\n/g, '\n').split('\n');
+
+    result.sort( (a, b) => {
+        return new Date(a.match(dateRegex)[0]) - new Date(b.match(dateRegex)[0]);
+    });
+
+    let guards = {};
+    let currentGuardId;
+    let laziestGuard;
+    let minAsleep = 0;
+    let minAwake = 0;
+    
+
+    for (let i = 0; i < result.length; i++) {
+        let date = new Date(result[i].match(dateRegex)[0]);
+        let minute = date.getMinutes();
+        let action = result[i].match(actionRegex)[0];
+        currentGuardId = result[i].match(idRegex) ? result[i].match(idRegex)[0] : currentGuardId;
+        if (action === 'falls asleep') {
+            minAsleep = minute;
+        } else if (action === 'wakes up') {
+            minAwake = minute;
+            for (let i = minAsleep; i < minAwake; i++) {
+                if (!guards[currentGuardId].sleepMinuteCount[i]) {
+                    guards[currentGuardId].sleepMinuteCount[i] = 1;
+                }
+                guards[currentGuardId].sleepMinuteCount[i]++;
+                guards[currentGuardId].minutesSlept++;
+                if (!guards[currentGuardId].gcsm)
+                    guards[currentGuardId].gcsm = i;
+                if (guards[currentGuardId].sleepMinuteCount[i] > guards[currentGuardId].sleepMinuteCount[guards[currentGuardId].gcsm])
+                    guards[currentGuardId].gcsm = i;
+            }
+
+            if (!laziestGuard) {
+                laziestGuard = guards[currentGuardId];
+            } else {
+                if (laziestGuard.minutesSlept < guards[currentGuardId].minutesSlept)
+                    laziestGuard = guards[currentGuardId];
+            }
+        } else {
+            if (!guards[currentGuardId]) {
+                guards[currentGuardId] = {
+                    id: currentGuardId,
+                    sleepMinuteCount: {},
+                    gcsm: '',
+                    minutesSlept: 0
+                }
+            }
+        }
+    }
+    console.log(laziestGuard.gcsm * laziestGuard.id);
+    return laziestGuard.gcsm * laziestGuard.id;
+});
