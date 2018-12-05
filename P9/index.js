e6ed96040b48e85b90d5ede39edb23461ed8eb0d
Wilton Ngo <wiltonngo@gmail.com>
2018-12-05 00:47:35 -0500

    Added solution to P9

---
+++ P9/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+let input = "input.txt";
+
+fs.readFile(input, 'utf8', function(err, data) {
+    if (err) throw err;
+    let result = data;
+    let stack = [];
+    for (let i = 0; i < result.length; i++) {
+        if (isPolarOpposites(stack[stack.length - 1], result[i])) {
+            stack.pop();
+        } else {
+            stack.push(result[i]);
+        }
+    }
+    console.log(stack.length);
+    return stack;
+});
+
+function isPolarOpposites(unit1, unit2) {
+    if (!unit1)
+        return false;
+    return (unit1.toUpperCase() === unit2 && unit2.toLowerCase() === unit1) || 
+           (unit1.toLowerCase() === unit2 && unit2.toUpperCase() === unit1);
+}
+
+/*
+a a
+a A
+A a
+A A
+*/
